9c8a807b9ff59fb6d80f6385f05ad7100e14d528
krishnanx <abhishekkrishnan2006@gmail.com>
2025-04-04 22:29:31 +0530

    header added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
+      appBar: AppBar(
+        toolbarHeight: 80, // Increase height if needed
+
+        flexibleSpace: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20.0, left: 16.0, right: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      child: Row(
+                        children: [
+                          SvgPicture.asset(
+                            'assets/icons/Sun.svg',
+                            width: 18,
+                            height: 18,
+                          ),
+                          const SizedBox(width: 5),
+                          Text(
+                            "Good Morning",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ), // Bold text),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      "Krishnan E",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/icons/Buy.svg',
+                      width: 24,
+                      height: 24,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       body: const Center(child: Text("Welcome to Home screen")),
     );
   }
